e5f615d3978f0f162f69fad540b596489449b3fb
4rjunc <arjunmundamani@gmail.com>
2025-03-01 00:09:01 +0530

    Deployed, Client Setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # solana-hello-api
+Program : [Here](https://explorer.solana.com/address/9uVWMyg6PGuLwWUYtA3vY2ubt6JiY5gJRcbHhCW4cXVz?cluster=devnet)
--- programs/solana-hello-api/src/lib.rs
+++ programs/solana-hello-api/src/lib.rs
@@ -5,12 +5,11 @@ declare_id!("9uVWMyg6PGuLwWUYtA3vY2ubt6JiY5gJRcbHhCW4cXVz");
 #[program]
 pub mod solana_hello_api {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn hello(_ctx: Context<Hello>) -> Result<()> {
+        msg!("Hello. World");
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Hello {}
